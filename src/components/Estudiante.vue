faadcf82c9e299f47779fcf15e110f28291b0d2b
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-05-15 23:29:34 -0500

    Deber 3 Formulario  estudiante Vue

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="container">
-        <label for="id_nombre">Nombre: </label>
-        <input id="id_nombre" type="text" v-model="nuevoNombre">
-        {{nuevoNombre}}
-        <label for="id_apellido">Apellido: </label>
-        <input id="id_apellido" type="text" v-model="nuevoApellido">
-        {{nuevoApellido}}
-        <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+        <form >
+            <label for="id_nombre">Nombre: </label>
+            <input id="id_nombre" type="text" v-model="nuevoNombre">
+            {{nuevoNombre}}<br/>
+            <label for="id_apellido">Apellido: </label>
+            <input id="id_apellido" type="text" v-model="nuevoApellido">
+            {{nuevoApellido}}<br>
+            <label for="id_edad">Edad: </label>
+            <input id="id_edad" type="number" v-model="nuevaEdad">
+            {{nuevaEdad}}<br>
+            <label for="id_genero">Género: </label>
+            <input id="id_genero" type="text" v-model="nuevoGenero">
+            {{nuevoGenero}}<br>
+            <label for="id_telefono">Teléfono: </label>
+            <input id="id_telefono" type="text" v-model="nuevoTelefono">
+            {{nuevoTelefono}}<br>
+            <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+        </form>
         <ul>
-            <li v-for="{nombre, apellido} in lista" :key="nombre"> Nombre: {{nombre}} - Apellido: {{apellido}} </li>
+            <li v-for="{nombre, apellido, edad, genero, telefono} in lista" :key="nombre"> Nombre: {{nombre}} - Apellido: {{apellido}} - edad: {{ edad }} - género: {{ genero }} - teléfono: {{ telefono }}</li>
         </ul>
     </div>  
 </template>
@@ -19,14 +30,17 @@ export default {
         return{
             nuevoNombre: '',
             nuevoApellido: '',
+            nuevaEdad: 0,
+            nuevoGenero: '',
+            nuevoTelefono: '',
             lista: [
-                {nombre:"John", apellido:"Quinatoa"},
-                {nombre:"Steven", apellido:"Guerrero"},
-                {nombre:"Naidelin", apellido:"Tabango"},
-                {nombre:"Brigith", apellido:"Mañay"},
-                {nombre:"Carmen", apellido:"Guerrero"},
-                {nombre:"Maria", apellido:"Puetate"},
-                {nombre:"Marco", apellido:"Quinatoa"},
+                {nombre:"John", apellido:"Quinatoa", edad:23, genero: "masculino", telefono:"09987215076"},
+                {nombre:"Steven", apellido:"Guerrero", edad:23, genero: "masculino", telefono:"0995369874"},
+                {nombre:"Naidelin", apellido:"Tabango", edad:21, genero: "femenino", telefono:"0991239856"},
+                {nombre:"Brigith", apellido:"Mañay", edad:21, genero: "femenino", telefono:"0991567894"},
+                {nombre:"Carmen", apellido:"Guerrero", edad:40, genero: "femenino", telefono:"09914562369"},
+                {nombre:"Maria", apellido:"Puetate", edad:40, genero: "femenino", telefono:"0991234785"},
+                {nombre:"Marco", apellido:"Quinatoa", edad:45, genero: "masculino", telefono:"0991985632"},
             ]
         }
     },
@@ -34,7 +48,10 @@ export default {
         agregarEstudiante(){
             const nuevo = {
                 nombre: this.nuevoNombre,
-                apellido: this.nuevoApellido
+                apellido: this.nuevoApellido,
+                edad: this.nuevaEdad,
+                genero: this.nuevoGenero,
+                telefono: this.nuevoTelefono
             }
             /* this.lista.unshift(nuevo); */
             this.lista.push(nuevo);
